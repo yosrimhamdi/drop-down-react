b01f04d0e500142ce5e178bb328dd2c78902a2a4
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-09 11:35:51 +0100

    implemented the app goal functinality

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Bellow from './Bellow';
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(-1);
+
   const renderedBellows = items.map(item => (
-    <Bellow key={item.id} item={item} />
+    <Bellow
+      key={item.id}
+      item={item}
+      activeIndex={activeIndex}
+      setActiveIndex={setActiveIndex}
+    />
   ));
+
+  console.log('will rerender');
 
   return <div>{renderedBellows}</div>;
 };
--- src/Bellow.js
+++ src/Bellow.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classnames from 'classnames';
 
 import './Bellow.sass';
 
-const Bellow = ({ item: { question, answer } }) => {
-  const [active, setActive] = useState(false);
-
+const Bellow = ({
+  item: { id, question, answer },
+  activeIndex,
+  setActiveIndex,
+}) => {
   const className = classnames({
     'bellow': true,
-    'bellow--active': active,
+    'bellow--active': activeIndex === id,
   });
 
   return (
-    <div className={className} onClick={() => setActive(!active)}>
+    <div className={className} onClick={() => setActiveIndex(id)}>
       <h2 className="bellow__question">{question}</h2>
       <div className="bellow__answer">{answer}</div>
     </div>
