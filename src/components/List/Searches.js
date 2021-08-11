ac9962db09f411ef22abba2ebcec2f5b3e9ae754
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-11 15:17:27 +0100

    removed nasty spans using RegEx.

--- src/components/List/Searches.js
+++ src/components/List/Searches.js
@@ -3,12 +3,16 @@ import React from 'react';
 const Searches = ({ searches }) => {
   console.log('will render Searches');
 
-  const renderedSearches = searches.map(({ pageid, title, snippet }) => (
-    <div key={pageid}>
-      <h1>{title}</h1>
-      <p>{snippet}</p>
-    </div>
-  ));
+  const renderedSearches = searches.map(({ pageid, title, snippet }) => {
+    snippet = snippet.replace(/(<span class="searchmatch">|<\/span>)/g, '');
+
+    return (
+      <div key={pageid}>
+        <h1>{title}</h1>
+        <p>{snippet}</p>
+      </div>
+    );
+  });
 
   return <div>{renderedSearches}</div>;
 };
