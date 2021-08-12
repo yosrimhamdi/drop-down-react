cf60418dbd5a91e2be92e4b4fec0d23ace9e3b46
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-12 17:14:40 +0100

    delted a comment.

--- src/api/wikipedia.js
+++ src/api/wikipedia.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-// https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=programming
-
 export default axios.create({
   baseURL: 'https://en.wikipedia.org/w/api.php',
   params: {
